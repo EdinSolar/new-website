462941d8fb3a9dada92dcc561d05795205ffe653
William Mathewson <wncmathewson@me.com>
2015-02-14 13:41:06 +0000

    New migration to add users to the db

---
+++ db/migrate/20150214133427_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :username, unique: true, index: true
+      t.string :full_name
+      t.string :email_address, unique: true, index: true
+      t.string :password_digest
+      t.string :password_salt
+      t.timestamps null: false
+
+      t.refences :role, index: true
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
